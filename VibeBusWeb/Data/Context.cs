e2dc302c619168868aafceaa4b176a6b287c9652
Артур <@DESKTOP-0ISUU4Q>
2024-04-29 00:25:53 +0300

    regBD

---
+++ VibeBusWeb/Data/Context.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VibeBusWeb.Data
+{
+    public class Buses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Number { get; set; } = null!;
+    }
+
+    public class Cities
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Region { get; set; } = null!;
+    }
+
+    public class Drivers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+
+    public class Routes
+    {
+        public int Id { get; set; }
+        public string DepartureTime { get; set; } = null!;
+        public string DestinationTime { get; set; } = null!;
+    }
+
+    public class Trips
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+    }
+
+    public class DbConnectionContext : DbContext
+    {
+        public DbConnectionContext(DbContextOptions<DbConnectionContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Buses> Buses { get; set; }
+        public DbSet<Cities> Cities { get; set; }
+        public DbSet<Drivers> Drivers { get; set; }
+        public DbSet<Routes> Routes { get; set; }
+        public DbSet<Trips> Trips { get; set; }
+        public DbSet<Users> Users { get; set; }
+
+    }
+}
--- VibeBusWeb/Program.cs
+++ VibeBusWeb/Program.cs
@@ -1,21 +1,22 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using VibeBusWeb.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<DbConnectionContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
